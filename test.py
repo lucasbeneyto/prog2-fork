f7a4a55dc3f8ea5acee749f9d47445dcfd419315
lucasbeneyto <lbs62@alu.ua.es>
2025-02-24 14:23:36 +0100

    Test

---
+++ test.py
@@ -0,0 +1,7 @@
+from estudiante import Estudiante
+
+est1 = Estudiante('Juan Perez',20,['Matematicas','Historia'])
+est2 = Estudiante('Juan Calvo',21,['Fisica','Historia'])
+
+est1.inscribir_curso('Fisica')
+est1.inscribir_curso('Matematicas')
