d146bd217c593ca4b2df10269851ba2066d7ae8e
apg260 <apg260@alu.ua.es>
2025-02-24 14:12:54 +0100

    Update curso.py

--- curso.py
+++ curso.py
@@ -6,3 +6,6 @@ class Curso:
         self.codigo = codigo
         self.descripcion = descripcion
         Curso.total_cursos += 1  # Aumenta el contador cada vez que se crea un curso
+    def mostrar_detalles(self):
+        pass
+    
